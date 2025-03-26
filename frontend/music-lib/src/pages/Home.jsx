add4be6477ed74df2e51c6c5fc0ed11011cb70f1
Oxhaider <arulselvan2244@gmail.com>
2025-03-27 00:53:42 +0530

    Update landing page

--- frontend/music-lib/src/pages/Home.jsx
+++ frontend/music-lib/src/pages/Home.jsx
@@ -1,19 +1,35 @@
-import Button from '../components/Button';
-
+import React from 'react';
+import '../styles/Home.css';
 
 function Home() {
-
     console.log("Home is rendering!!");
 
+    return (
+        <div className="home-container">
+            <div className="home-content">
+                <p className="home-title">Discover Your Perfect Sound</p>
+                
+                <p className="home-description">
+                    Access millions of songs, create personalized playlists, and explore new music 
+                    tailored to your unique taste. Your entire music world, all in one place.
+                </p>
+                
+                
+                <div className="home-cta">
+                    <button className="get-started-button"> Get Started </button>
+                </div>
+            </div>
+            
 
-    return(
-        <div>
-        <h1>Hello this is a Task!!!!</h1>
-        <Button text="Logout"/>
+            <div className="home-image-container">
+                <img 
+                    src="/pic.jpg" 
+                    alt="Music Library" 
+                    className="home-image"
+                />
+            </div>
         </div>
-    )
-
+    );
 }
-
 
 export default Home;
